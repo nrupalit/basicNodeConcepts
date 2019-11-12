2eb2c2a91bdfd71a4ae69e0dc9e69376c29aabd0
Nrupalit <nrupali1997@gmail.com>
2019-11-12 12:46:54 +0530

    Additional Files

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [{
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    status: 'active'
+},{
+    id: 2,
+    name: 'Bob Williams',
+    email: 'bob@gmail.com',
+    status: 'inactive'
+},{
+    id: 3,
+    name: 'Shannon Jackson',
+    email: 'shannon@gmail.com',
+    status: 'inactive'
+},{
+    id: 4,
+    name: 'Bob Martin',
+    email: 'bobm@gmail.com',
+    status: 'active'
+},{
+    id: 5,
+    name: 'Geofery Hilton',
+    email: 'geofery@gmail.com',
+    status: 'active'
+}];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const path = require('path');
+const members = require('./Members.js');
 
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log(`id:${members.id}`);
+    next();
+}
+
+//Init Middlewear
+app.use(logger);
+
+//Gets All members
+app.get('/api/members', (req, res) => {
+    res.json(members);
+})
+
+//Set a static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000);
+app.listen(PORT);
